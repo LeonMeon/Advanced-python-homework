e87833ec67178b6ea0f3ae79d72b893491c4a214
LeonMeon <leonoff_99@mail.ru>
2022-09-13 12:52:44 +0300

    first commit

---
+++ stem_framework/stem/meta.py
@@ -0,0 +1,8 @@
+'''
+conception of metadata and metadata processor
+metadata - user-defined tree of values (to cache analysis
+results or send instructions to remote computing node)
+metadata processor principle - during the
+processing of data, only immutable initial data and metadata are allowed to be used as input -> declarative
+approach of describing data (no scripts, processing description in the form of metadata)
+'''
---
+++ stem_framework/stem/workspace.py
@@ -0,0 +1,6 @@
+'''
+conception modularity software
+The core of the system contains
+only basic functionality for working with metadata and organizing the data processing, all specific
+functions are placed in separate plugins
+'''
