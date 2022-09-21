937aca7808c567e4819330b5f4d8990914a97a96
LeonMeon <leonoff_99@mail.ru>
2022-09-22 00:40:53 +0300

    finally

--- stem_framework/stem/meta.py
+++ stem_framework/stem/meta.py
@@ -52,26 +52,62 @@ class MetaVerification:
         
     # verify meta by specification. Raise SpecificationError if verification impossible.
     #Metadata verification: each task has Specification which describes required meta and the metadata processor checks input meta the correspondence to the specification.
-    
+    # I didn't get it at first (and second, and third) but then I got explained what I need to do here
     @staticmethod
     def verify(meta: Meta,
                specification: Optional[Specification] = None) -> "MetaVerification":
-        for meta_value , spec_value in zip(meta.values(), specification.values())
-            try:
-                if isinstance(meta_value in, spec_value):
-                    print(spec_value)
+        #keys
+        spec = specification
+        meta_keys = meta.__dataclass_fields__.keys() if is_dataclass(meta) else dict(meta.keys())
+        spec_keys = spec.__dataclass_fields__.keys() if is_dataclass(spec) else dict(spec).keys()
+        
+        for spec_key in spec_keys:
+            spec_type = spec.__dataclass_fields__[spec_key].type if is_dataclass(spec) else spec[spec_key]
+            if spec_key not in meta_keys:
+                self.error.append(MetaFieldError(required_key = spec_key, required_types = spec_type ) )
+            else:
+                presented_value = get_meta_attr(meta, required_key)
+                presented_type = type(presented_value)
+                # Oh, I should count recursion. Painful
+                if (isinstance(spec_type, type) or (
+                    isinstance(spec_type, tuple) and isinstance(spec_type[0], type)
+                )):
+                    # types checking
+                    if not issubclass(presented_type, spec_type):
+                        errors.append( MetaFieldError(
+                                required_key = spec_key,
+                                required_types = spec_type,
+                                presented_value = presented_value,
+                                presented_type = presented_type ) )
                 else:
-                    print(type(meta_value))            
-            except:
-                raise SpecificationError
+                    # deeper ((())) (()) ()
+                    errors_next_level = MetaVerification.verify(
+                        get_meta_attr(meta, required_key),
+                        required_types).error
+
+                    if errors_next_level != ():
+                        errors.append(errors_next_level)
+
+        return MetaVerification(*errors)
+            
+
+
 
 #3.(1 p.) get_meta_attr(meta : Meta, key : str, default : Optional[Any] = None) -> Optional[Any]: which return meta value by key from top level of meta or default if key don't exist in meta
 def get_meta_attr(meta : Meta, key : str, default : Optional[Any] = None) -> Optional[Any]:
     if hasattr(meta, key):
         return getattr(meta, key)
+    try:
+        return meta.get(key,default)
+    except:
+        return None
     return default
 
 #4.(1 p.) function def update_meta(meta: Meta, **kwargs): which update meta from kwargs.
 def update_meta(meta: Meta, **kwargs):
-    for key in kwargs:
-        setattr(meta, key, kwargs[key])  
+    if is_dataclass(meta):
+        for key in kwargs:
+            setattr(meta, key, kwargs[key])  
+    else:
+        for key in kwargs:
+            meta.update(kwargs, kwargs[key])
