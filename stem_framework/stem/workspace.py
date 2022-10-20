75ee4608edda2fa57e34e53d2695a969900cf34b
LeonMeon <leonoff_99@mail.ru>
2022-10-20 17:58:09 +0300

    corrected 4th hw

--- stem_framework/stem/workspace.py
+++ stem_framework/stem/workspace.py
@@ -7,7 +7,8 @@ functions are placed in separate plugins
 
 from abc import abstractmethod, ABC, ABCMeta
 from types import ModuleType
-from typing import Optional, Any, TypeVar, Union
+from typing import Optional, Any, TypeVar, Union, Type
+from importlib import import_module
 
 from .core import Named
 from .meta import Meta
@@ -42,8 +43,8 @@ class TaskPath:
     def __str__(self):
         return ".".join(self._path)
 
-
-class ProxyTask(Task[T]): #The constructor of class ProxyTask receive two arguments: some task object and it proxy name
+#The constructor of class ProxyTask receive two arguments: some task object and it proxy name
+class ProxyTask(Task[T]): 
 
     def __init__(self, proxy_name, task: Task):
         self._name = proxy_name
@@ -79,6 +80,7 @@ class IWorkspace(ABC, Named):
     # return task from this workspace or from his sub-workspaces
     #task_path is special TaskPath object or string in next format
     def find_task(self, task_path: Union[str, TaskPath]) -> Optional[Task]:
+        
         if not isinstance(task_path, TaskPath):
             task_path = TaskPath(task_path)
         
@@ -86,7 +88,7 @@ class IWorkspace(ABC, Named):
         if not task_path.is_leaf: # from TaskPath description : len(self._path) == 1
             for work_space in self.workspaces: # find appropriate head
                 if work_space.name == task_path.head: # from TaskPath description  :self._path[0]
-                    return work_space.find_task(task_path.sub_path) # from TaskPath description: TaskPath(self._path[1:])
+                    return work_space.find_task(task_path.sub_path) # TaskPath(self._path[1:])
             return None
         # leaf case
         else:
@@ -98,47 +100,49 @@ class IWorkspace(ABC, Named):
                 if work_space.find_task(task_path) is not None:
                     return work_space.find_task(task_path)
             return None
-                
+                  
     def has_task(self, task_path: Union[str, TaskPath]) -> bool:
         return self.find_task(task_path) is not None
-
+    
     def get_workspace(self, name) -> Optional["IWorkspace"]:
         for workspace in self.workspaces:
             if workspace.name == name:
                 return workspace
         return None
-
+    
     def structure(self) -> dict:
         return {
             "name": self.name,
             "tasks": list(self.tasks.keys()),
-            "workspaces": [w.structure() for w in self.workspaces]
+            "workspaces": [workspace.structure() for workspace in self.workspaces]
         }
     
     #return _stem_workspace value if it exists and module-workspace for module which contains task definition.
     @staticmethod
-    def find_default_workspace(task: Task) -> "IWorkspace":  
-        if hasattr(task, "_stem_workspace"): #if this variable exists, else create module-workspace and save it in this variable
-            return getattr(task, "_stem_workspace")
+    def find_default_workspace(task: Task) -> Type["IWorkspace"]:
+         #if this variable exists, else create module-workspace and save it in this variable
+        if hasattr(task, "_stem_workspace"):
+            return task._stem_workspace
         else: 
             return IWorkspace.module_workspace(import_module(task.__module__))
     
     # return module-workspaces:
     # instance of LocalWorkspace, which contain tasks (instance of Task or its subclasses) and workspaces (instance of IWorkspace and its subclasses) defined in module associated with variable module
     @staticmethod
-    def module_workspace(module: ModuleType) -> "IWorkspace":
-        if hasattr(module, "_stem_workspace"):  
-            return getattr(module, "_stem_workspace")
-        else:  # else create module-workspace and save it in this variable         
+    def module_workspace(module: ModuleType) -> Type["IWorkspace"]:      
+        try: #if hasattr(module, "_stem_workspace"): 
+            return module.__stem_workspace
+        # else create module-workspace and save it in this variable         
+        except AttributeError: #else:
             tasks, workspaces = {}, []
             for attr in dir(module):
-                cls = getattr(module, attr)
-                if isinstance(cls, Task): # which contain tasks
-                    tasks[attr] = cls
-                if isinstance(cls, IWorkspace): # and workspaces
-                    workspaces.append(cls)
+                _attr = getattr(module, attr)
+                if isinstance(_attr, Task): # which contain tasks
+                    tasks[attr] = _attr
+                if isinstance(_attr, type) and issubclass(_attr, IWorkspace): # and workspaces
+                    workspaces.append(_attr)
             setattr(module, "_stem_workspace", LocalWorkspace(module.__name__, tasks, workspaces)) 
-            return getattr(module, "_stem_workspace")
+            return module.__stem_workspace
 
 class ILocalWorkspace(IWorkspace):
 
@@ -161,16 +165,19 @@ class LocalWorkspace(ILocalWorkspace):
 
 class Workspace(ABCMeta, ILocalWorkspace):
     def __new__(mcs, name, interfaces , attrs, **kwargs):
-        cls = super().__new__(ABCMeta, name, interfaces , attrs, **kwargs) # must be returned on constructor call of user classes.
+        # must be returned on constructor call of user classes.
+        cls = super().__new__(ABCMeta, name, interfaces , attrs, **kwargs) 
         try: 
             workspaces = set(cls.workspaces) #must be converted to set if present 
         except AttributeError:
             workspaces = set() #else property workspaces must be return empty set
 
         if ILocalWorkspace not in interfaces:
-            interfaces += (ILocalWorkspace,) # Class-objects of user classes implement the interface ILocalWorkspace
+            # Class-objects of user classes implement the interface ILocalWorkspace
+            interfaces += (ILocalWorkspace,) 
             
-        cls = super().__new__(mcs, name, interfaces, attrs, **kwargs) # must be returned on constructor call of user classes.
+        # must be returned on constructor call of user classes.    
+        cls = super().__new__(mcs, name, interfaces, attrs, **kwargs) 
         cls_attr = {task_name: task for task_name, task in cls.__dict__.items()}
         
         # iii All attributes of the user workspace class must be replaced on ProxyTask objects
@@ -185,7 +192,7 @@ class Workspace(ABCMeta, ILocalWorkspace):
                 task._stem_workspace = mcs # v All tasks must have it
         
         #return dictionary, which have names of tasks in keys and itself tasks in values
-        tasks = {task_name: task for task_name, task in cls.__dict__.items() if isinstance(task, Task)} # iv
+        tasks = {task_name: task for task_name, task in cls.__dict__.items() if isinstance(task, Task)} 
         
         cls._workspaces, cls._tasks, cls._name  = workspaces, tasks, name
         return cls
